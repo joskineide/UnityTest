a54d21ad001b4bc08ab3a8f0e1cfa8020a8f5170
Tharcisio <tharcisio_alves@outlook.com>
2020-09-12 16:31:40 -0300

    Menus

---
+++ Assets/Scripts/HudScripts/ButtonManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject menu;
+ 
+    public void DeactivateMenu() 
+    {
+        menu.SetActive(false);
+    }
+}
--- Assets/Scripts/ProjectyleScript.cs
+++ Assets/Scripts/ProjectyleScript.cs
@@ -34,7 +34,10 @@ public class ProjectyleScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.GetComponent<EnemyScript>()){
             other.GetComponent<EnemyScript>()
-                .takeDamage(dealDamage());
+                .takeDamage(1);
+            pierce--;
+            if (pierce < 1)
+                Destroy(this.gameObject);
         }
     }
 }
